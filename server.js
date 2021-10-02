50c830e6924f8b0857525bed82c7b66c1660d508
kelseygasser <kgasser@payitgov.com>
2021-10-02 01:14:43 -0500

    fixing scripts

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('./dist/pokedex'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/pokedex/'}),
 );
 
 app.listen(process.env.PORT || 8080);
